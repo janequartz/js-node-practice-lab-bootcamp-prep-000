3a5972ee7e75450fe75318fe89722cccd985a99e
verdantkyber <kingjalexa@gmail.com>
2019-03-22 07:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // write your code below!
 
+<<<<<<< HEAD
 var name = "Echo"
 var height = 72
+=======
+var "name" = "Joe"
+var height = "74"
+>>>>>>> f31deef87d7bb424936300938125067c730e9a2b
 
 console.log("Name:")
 console.log(name)
